9bb514194bc4faed17cbfb5fe36dce126b16bd2e
najam <najam.ahmad015@gmail.com>
2022-05-14 01:31:52 +0500

    wo show man and chars

--- script.js
+++ script.js
@@ -7,12 +7,11 @@ let keyPressSound = new Audio('assets/keyPress.wav');
 //controllers btns
 hint = document.getElementById('hint');
 playAgain = document.getElementById('restart');
-const wrapper = document.querySelector('#chosenCategary')
 
 //body parts
 let hang = document.getElementById('hang');
 let head = document.getElementById('head');
-let neck = document.getElementById('neck');
+let neck = document.getElementById('neack');
 let rightHand = document.getElementById('righthand');
 let leftHand = document.getElementById('lefthand');
 let rightLeg = document.getElementById('rightleg');
@@ -21,23 +20,37 @@ let danda = document.getElementById('danda');
 let stand = document.getElementById('stand');
 let hangStand = document.getElementById('hangStand');
 
+const wrapper = document.querySelector('#chosenCategary')
 let attemps = document.querySelector('#attemps');
 let result = document.getElementById('chosenCategary');
 let Clue = document.getElementById('Clue');
 
 words = ['milk','good','bad','number','learning','gaming','element','html','javascript']
-parts = ['stand','danda','leftLeg','rightLeg','leftHand','rightHand','neck','head','hang','hangStand']
 
+let winCount = '';
 let counter = 0;
 let retry = 10;
 let userPressedWord = '';
-let string = '';
-console.log(string)
+let randomNum = '';
+console.log(randomNum)
+
+parts = [
+    stand,
+    danda,
+    hangStand,
+    hang,
+    head,
+    neck,
+    leftHand,
+    rightHand,
+    leftLeg,
+    rightLeg
+]
 
 generateGame();
 
-function generateElements(string){
-    var chars = string.split('');
+function generateElements(randomNum){
+    let chars = randomNum.split('');
     for(i=0; i<chars.length; i++){
         var div = document.createElement('div');
         var char = chars[i];
@@ -50,32 +63,34 @@ function generateElements(string){
 
 function generateGame(){
   var index = Math.ceil(Math.random() * (words.length - 1))
-  string = words[index];
-  generateElements(string)
-  console.log(string)
+  randomNum = words[index];
+  console.log('Random WOrd is:', randomNum)
+  generateElements(randomNum)
 }
 
-function showHidenChars(string){
-    var chars = string.split('');
-    for(i = 0; i < chars.length; i++){
-        var char = chars[i];
-        var div = document.getElementsByClassName(char.toLowerCase());
-        div.classList.add("white")
-        wrapper.appendChild(div);
-    }
+function showHidenChars(randomNum, userPressedWord){
+        var check = randomNum.includes(userPressedWord);
+        
+        if(check){
+            var char = document.getElementsByClassName(userPressedWord);
 
+            for ( j = 0; j < char.length; j++) {
+                char[j].classList.add('chita');
+              }
+        }
 }
 
 function typeWord(w){
     keyPressSound.play();
     userPressedWord = w;
-    console.log('user pressed', userPressedWord);
-    isStringIncludes(userPressedWord)
+
+    console.log('user pressed', userPressedWord, userPressedWord.length);
+    isStringIncludes(userPressedWord);
+
 }
 
 function shuffle(s) {
     var arr = s.split('');           
-
     arr.sort(function() {
       return 0.5 - Math.random();
     });
@@ -83,41 +98,57 @@ function shuffle(s) {
     return s;                        
   }
 
-  function showHint(string){
+  function showHint(randomNum){
     hintSound.play();
-    var s = string;
+    var s = randomNum;
     s = shuffle(s);
     Clue.innerHTML = s;
   }
 
 function isStringIncludes(userPressedWord) {
-    var check = string.includes(userPressedWord);
-    console.log('Random WOrd is', string)
-    console.log('najam is testing',check)
+    var check = randomNum.includes(userPressedWord);
+    console.log('Random WOrd is:', randomNum)
+    console.log('user pressed:', userPressedWord, ' = ' ,check)
 
-    if(check == true){
-        showHidenChars(string);
+    if(check){
+        showHidenChars(randomNum, userPressedWord);
     }
     else{
-        retry -=1;
-        showManPart()
-        console.log('Retry Left Back', retry)
+        retry--;
         attemps.innerHTML = "Remaning Attemps:"+ retry;
+        showManPart();
     }
-
 }
 
 function showManPart() {
-    a = parts[counter];
-    console.log(a);
-    TryRemainsBack()
+    remaningTries()
+    var part = parts[counter];
     counter ++;
+    part.classList.add("white");
 }
 
-function TryRemainsBack(){
+function remaningTries(){
     if(retry <= 0 ){
         dieSound.play();
         Clue.innerHTML = 'You Lose Game!';
+    }
+}
+
+function winGame(randomNum){
+    let chars = randomNum.split('');
+    console.log("chars lenth = ", chars.length)
+    if(chars.length == winCount ){
+        winSound.play();
+        Clue.innerHTML = 'You Win The Game!';
+    }
+    
+}
+
+function disableBtn(userPressedWord){
+    if(userPressedWord){
+        document.getElementById(userPressedWord.toLowerCase()).disabled = true;
+        winCount += userPressedWord.length;
+        console.log("win count = ", winCount);
     }
 
 }
